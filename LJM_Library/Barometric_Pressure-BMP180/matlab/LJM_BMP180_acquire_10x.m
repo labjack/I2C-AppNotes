751422ea3ee61a5a6508ec8012d87f1851f5bd7d
chrisjohn404 <chris.s.johnson4@gmail.com>
2015-12-11 15:00:05 -0700

    Added the LJM alternative to the acquire_10x example.

--- LJM_Library/Barometric_Pressure-BMP180/matlab/LJM_BMP180_acquire_10x.m
+++ LJM_Library/Barometric_Pressure-BMP180/matlab/LJM_BMP180_acquire_10x.m
@@ -1,4 +1,4 @@
-function result = UD_BMP180_acquire_10x()
+function result = LJM_BMP180_acquire_10x()
 	% This function is designed to verify that the the BMP180 acceelrometer is
 	% properly connected to the LabJack device.  Essentially, it reads the 
 	% Chip-id register from the I2C sensor and makes sure that it received the 
@@ -8,22 +8,20 @@ function result = UD_BMP180_acquire_10x()
 		clc % Clear the MATLAB command window
 		clear % Clear the MATLAB variables
 
-		% Make the UD .NET assembly visible in MATLAB
-		ljasm = NET.addAssembly('LJUDDotNet');
-		ljudObj = LabJack.LabJackUD.LJUD;
+		% Make the LJM .NET assembly visible in MATLAB
+		ljmAsm = NET.addAssembly('LabJack.LJM');
+		ljmType = ljmAsm.AssemblyHandle.GetType('LabJack.LJM');
+		ljmConstantsType = ljmAsm.AssemblyHandle.GetType('LabJack.LJM+CONSTANTS');
 
-		% Open the first found LabJack U3.
-		% disp('Opening U3');
-		% [ljerror, ljhandle] = ljudObj.OpenLabJack(LabJack.LabJackUD.DEVICE.U3,LabJack.LabJackUD.CONNECTION.USB,'0',true,0);
+		% Create an object to nested class LabJack.LJM.CONSTANTS
+		LJM_CONSTANTS = System.Activator.CreateInstance(ljmConstantsType);
+		LJM = System.Activator.CreateInstance(ljmType);
 
-		% Open the first found LabJack U6.
-		disp('Opening U6');
-		[ljerror, ljhandle] = ljudObj.OpenLabJack(LabJack.LabJackUD.DEVICE.U6,LabJack.LabJackUD.CONNECTION.USB,'0',true,0);
+		%Open first found LabJack
+		ljhandle=0;
+		[ljmError, ljhandle] = LJM.OpenS('LJM_dtT7', 'LJM_ctUSB', 'ANY', ljhandle);
+		showDeviceInfo(ljhandle);
 
-		% Open the first found LabJack UE9.
-		% disp('Opening UE9');
-		% [ljerror, ljhandle] = ljudObj.OpenLabJack(LabJack.LabJackUD.DEVICE.UE9,LabJack.LabJackUD.CONNECTION.USB,'0',true,0);		
-		
 		% Initialize the I2C Utility.
 		i2cUtils = UD_I2C_Utils(ljudObj, ljhandle);
 		i2cUtils.enable_debug = false;
